500b859ba9f616f18ed1b2822fa9a9943699fbb8
Juna Rachelle <blackstar.sexy>
2022-05-27 09:20:44 -0400

    added installer for ease of use to be used alongside packagebuild and removed graphicbased script to other project

--- configdgv.sh
+++
@@ -1,2 +0,0 @@
-cd /opt/ffxi/drive_c/WingsXI/Ashita/ffxi-bootmod
-WINEPREFIX=/opt/ffxi wine /opt/ffxi/drive_c/WingsXI/Ashita/ffxi-bootmod/dgVoodooCpl.exe 2>&1 1>/dev/null &
---
+++ installer.sh
@@ -0,0 +1,22 @@
+echo 'please make sure your wingsxi'
+echo 'zip file is in the Downloads/leetxi'
+echo 'directory of your user'
+echo 'make this directory if needed'
+echo 'press enter to continue...'
+read trash
+
+7z x ~/Downloads/leetxi/*.7z
+
+WINEPREFIX=/opt/ffxi wine Downloads/leetxi/Installer\ Package/Installer.exe
+
+echo 'running a short cleanup'
+
+cp /opt/ffxi/drive_c/WingsXI/Ashita/config/boot/New\ Configuration\ 1.xml /opt/ffxi/drive_c/WingsXI/Ashita/config/boot/wings.xml
+rm /opt/ffxi/drive_c/WingsXI/Ashita/config/boot/New\ Configuration\ 1.xml
+
+echo 'please be sure to run configure ashita'
+echo 'upon first launch of leetxi'
+echo 'let it update, and then close'
+echo 'using the launch command'
+echo 'as ashita gui has issues'
+echo 'in wine'
